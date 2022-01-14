4edc9aa80d8b69649585b2f4404b8c52559bb5b6
newjini <kimyjin0920@gmail.com>
2022-01-15 00:38:24 +0900

    feat: add Hello World

--- backend/fakebaemin/src/main/java/com/perfectimitation/fakebaemin/FakebaeminApplication.java
+++ backend/fakebaemin/src/main/java/com/perfectimitation/fakebaemin/FakebaeminApplication.java
@@ -2,12 +2,21 @@ package com.perfectimitation.fakebaemin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FakebaeminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FakebaeminApplication.class, args);
+	}
+	@GetMapping(path = "/")
+	@ResponseBody
+	public String home() {
+		return "Hello World";
 	}
 
 }
